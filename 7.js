0d7655c407c89579367304a032353f43167613d7
pavani562 <pavanigunji7@gmail.com>
2025-06-07 13:43:35 +0530

    Add files via upload

--- 7.js
+++ 7.js
@@ -10,7 +10,7 @@ function fetchUsers() {
     return;
   }
 
-  fetch('http://localhost:3000/users')
+  fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => {
       if (!response.ok) {
         throw new Error(`HTTP error: ${response.status}`);
